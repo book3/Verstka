8da51a5859c9875332e837bffe7c6d8ae264ee7b
Yntymak <y.karagulov@gmail.com>
2022-11-17 22:26:59 +0600

    Initial Commit

---
+++ Verstka/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Verstka
+//
+//  Created by Yntymak on 17/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
